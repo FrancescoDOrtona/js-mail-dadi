a8047505dec66d6dd5d3243a93b1e98239115c5e
FrancescoDOrtona <francescod.97@hotmail.it>
2023-10-09 15:37:33 +0200

    Add: JS Dice number generator & winner declaration

--- Dadi/js/app.js
+++ Dadi/js/app.js
@@ -1,1 +1,14 @@
 // # Gioco dei dadi
+// Dichiaro le variabili e genero un numero random per entrambi
+const userNumber = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+const pcNumber = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+console.log(userNumber, pcNumber)
+
+// confronto i 2 numeri e dichiaro il vincitore
+if (userNumber > pcNumber){
+    console.log('Hai vinto')
+} else if (userNumber < pcNumber){
+    console.log('Hai perso')
+} else if (userNumber === pcNumber){
+    console.log('Pareggio')
+}
