b088be850332f854f4bfafce1e8069b25e8159f2
FrancescoDOrtona <francescod.97@hotmail.it>
2023-10-09 15:02:54 +0200

    Add: for loop, allowed email or not

--- Mail/js/app.js
+++ Mail/js/app.js
@@ -1,20 +1,28 @@
 // # Email
 // - Chiedo all'utente l'email e recupero tramite id il valore inserito
 
-const emailDOMElement = document.getElementById('email')
-const btnDOMElement = document.getElementById('send-email')
+const emailDOMElement = document.getElementById('email');
+const btnDOMElement = document.getElementById('send-email');
 
 // - Dichiaro una variabile con un array delle email consentite
-const allowedEmails = ['batman@ghotam.com', 'peterparker@spiderman.com', 'luffy@onepiece.com', 'tanjiro@demonslayer.com', 'gojo@satoru.jp', 'akame@nightraid.jp']
-console.log(allowedEmails)
+const allowedEmails = ['batman@ghotam.com', 'peterparker@spiderman.com', 'luffy@onepiece.com', 'tanjiro@demonslayer.com', 'gojo@satoru.jp', 'akame@nightraid.jp'];
+console.log(allowedEmails);
 
-const arrayLenght = allowedEmails.length
-console.log(arrayLenght)
+const arrayLenght = allowedEmails.length;
+console.log(arrayLenght);
 
 // - Dichiaro un ciclo for che al click controllerà l'email inserita dall'utente se consente l'accesso
 btnDOMElement.addEventListener('click', function(){
-    const email = emailDOMElement.value
-    console.log(typeof(email),email, emailDOMElement)
+    const email = emailDOMElement.value;
+    console.log(typeof(email),email, emailDOMElement);
 
+    for(i = 0; i < arrayLenght; i++){
+        if(email === allowedEmails[i]){
+            console.log('Accesso Consentito')
+        } else 
+        console.log('Accesso Negato')
+        
     
+        
+    }
 })
